cf692e63980f78674e85209c2d21b0353805548f
HartelooA <96767184+HartelooA@users.noreply.github.com>
2022-01-07 14:53:12 -0800

    Adding browse for directory support

--- win32apitest.py
+++ win32apitest.py
@@ -1,6 +1,8 @@
 import string
 from time import sleep
 from ctypes import windll
+import tkinter as tk
+from tkinter import filefialog as fd
 import os
 import re
 
@@ -32,12 +34,20 @@ def config():
 			bitmask >>= 1
 
 		#Generate a list of lines to write to the file using writelines()
-		#This is okay for the single line config file
+		#This is okay for first single line config file
 		header = "static_drives = "
 		parameters = ", ".join(drives)
 		parameters = header + parameters
+		#Second line: prompts directory selection window for dest_path
+		header = "dest_path = "
+		dest = tk.fd.askdirectory()
+		#Append the second line into parameters list
+		parameters = parameters.append(header + dest)
+		#create file
 		cfg_file = open("config.cfg", "w+")
+		#print items in parameters as lines in file
 		cfg_file.writelines(parameters)
+		#Finally, grab static drives list so first drive check is correct
 		static_drives = drives
 
 		#For now, just static drives, elsewise entire dictionary used for Config file
